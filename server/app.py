2d2fd277c8b793e5666df1b0af3ad287624cec46
chous <to100100100@gmail.com>
2017-12-23 03:19:12 +0300

    Add mongodb and delete postgresql

--- server/app.py
+++ server/app.py
@@ -7,26 +7,15 @@ import jinja2
 import aiohttp_jinja2
 from aiohttp import web
 import aiohttp_debugtoolbar
+from motor import motor_asyncio
 
 from apps.chatter.routes import setup_routes
 import settings
 
 
 async def init_pg(app):
-    engine = await aiopg.sa.create_engine(
-        database=os.environ.get('POSTGRES_DB'),
-        user=os.environ.get('POSTGRES_USER'),
-        password=os.environ.get('POSTGRES_PASSWORD'),
-        host='db',
-        port='5432',
-        loop=app.loop,
-    )
-    app['db'] = engine
-
-
-async def close_pg(app):
-    app['db'].close()
-    await app['db'].wait_closed()
+    app.client = motor_asyncio.AsyncIOMotorClient(settings.MONGO_HOST)
+    app.db = app.client[settings.MONGO_DB_NAME]
 
 
 p = argparse.ArgumentParser()
@@ -34,12 +23,11 @@ p.add_argument('--host', required=False)
 p.add_argument('--port', required=False, type=int)
 
 opts, args = p.parse_known_args(sys.argv[1:])
-host, port, path = opts.host, opts.port, opts.path
+host, port = opts.host, opts.port
 
 app = web.Application()
 aiohttp_debugtoolbar.setup(app)
 app['config'] = {}
 aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader(searchpath=settings.TEMPLATE_PATH))
 app.on_startup.append(init_pg)
-app.on_cleanup.append(close_pg)
 setup_routes(app)
--- server/settings.py
+++ server/settings.py
@@ -1,10 +1,12 @@
 import os
 
 
-ROOT_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), '../..'))
-
+ROOT_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), '../'))
 SERVER_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
-
 CLIENT_DIR = os.path.normpath(os.path.join(ROOT_DIR, 'client'))
 
 TEMPLATE_PATH = os.path.normpath(os.path.join(CLIENT_DIR, 'src/templates'))
+
+MONGO_HOST = '127.0.0.1'
+MONGO_PORT = 27017
+MONGO_DB_NAME = 'chatter'
